e60ea1999ed53c66eaf35591d849c39b7298a5f8
kksltv123 <kksltv123@naver.com>
2022-08-03 20:45:31 +0900

    revise toolkit

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,29 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// Action value
-// const ADD_TODO = "ADD_TODO";
-// const TOGGLE = "TOGGLE";
-// const REMOVE = "REMOVE";
-// const GET_TODO_BY_ID = "GET_TODO_BY_ID";
-
-// // Action Creator
-// export const addTodo = text => ({
-//   type: ADD_TODO,
-//   text
-// });
-// export const toggle = id => ({
-//   type: TOGGLE,
-//   id
-// })
-// export const remove = id => ({
-//   type: REMOVE,
-//   id
-// })
-// // 상세 페이지에서 특정 Todo만 조회하는 action creator
-// export const getTodoByID = id => ({
-//     type: GET_TODO_BY_ID,
-//     id,
-// });
 
 
 // initial State
@@ -51,37 +27,6 @@ const initialState = {
   }
 };
 
-// Reducer
-// const todos = (state = initialState, action) => {
-//   switch (action.type) {
-//     case ADD_TODO:
-//       return {
-//         ...state,
-//         todos: [...state.todos, action.text],
-//       };
-//     case TOGGLE:
-//       return {
-//         ...state,
-//         todos: state.todos.map(todo =>
-//           todo.id === action.id ? { ...todo, isDone: !todo.isDone } : todo
-//         )
-//       }
-//     case REMOVE:
-//       return {
-//         ...state,
-//         todos: state.todos.filter(todo => todo.id !== action.id)
-//       };
-//     case GET_TODO_BY_ID:
-//       return {
-//         ...state,
-//         todo: state.todos.find((todo) => {
-//           return todo.id === action.id;
-//         }),
-//       };
-//     default:
-//       return state;
-//   }
-// };
 
 
 const todosSlice = createSlice({
